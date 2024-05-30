da61f00b6a88d6b843cc85eb487c128c3ca4d7a2
offsec <liguofeng@boprin.onmicrosoft.com>
2024-05-30 22:23:07 +0800

    Initial commit

---
+++ gov_pdf_scraper/target/debug/build/proc-macro2-89244a3c36e39423/out/proc_macro2.d
@@ -0,0 +1,7 @@
+/Users/offsec/Desktop/gov_pdf_scraper/target/debug/build/proc-macro2-89244a3c36e39423/out/libproc_macro2.rmeta: build/probe.rs
+
+/Users/offsec/Desktop/gov_pdf_scraper/target/debug/build/proc-macro2-89244a3c36e39423/out/proc_macro2.d: build/probe.rs
+
+build/probe.rs:
+
+# env-dep:RUSTC_BOOTSTRAP
